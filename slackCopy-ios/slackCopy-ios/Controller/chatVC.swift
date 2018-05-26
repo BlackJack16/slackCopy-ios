47a5976af23bd7a98d355ad3fc43e52bdd332017
priyanto <16priyanto@gmail.com>
2018-05-26 18:29:17 +0700

    add tab and pan gestures

--- slackCopy-ios/slackCopy-ios/Controller/chatVC.swift
+++ slackCopy-ios/slackCopy-ios/Controller/chatVC.swift
@@ -16,7 +16,8 @@ class chatVC: UIViewController {
         super.viewDidLoad()
      
         menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
-
+        self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
     }
     
 }
