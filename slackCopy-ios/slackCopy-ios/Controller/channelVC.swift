516e1559443fb179dd1b63c92f431ada0dac3104
priyanto <16priyanto@gmail.com>
2018-05-26 23:54:32 +0700

    create loginVC

--- slackCopy-ios/slackCopy-ios/Controller/channelVC.swift
+++ slackCopy-ios/slackCopy-ios/Controller/channelVC.swift
@@ -10,10 +10,14 @@ import UIKit
 
 class channelVC: UIViewController {
 
+    @IBOutlet weak var btnLogin: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 60
     }
 
+    @IBAction func btnLoginPressed(_ sender: Any) {
+        performSegue(withIdentifier: TO_LOGIN, sender: nil)
+    }
 }
---
+++ slackCopy-ios/slackCopy-ios/Utillities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  slackCopy-ios
+//
+//  Created by Black Jack on 26/05/18.
+//  Copyright © 2018 priyanto. All rights reserved.
+//
+
+import Foundation
+
+//Segues
+let TO_LOGIN = "toLogin"
