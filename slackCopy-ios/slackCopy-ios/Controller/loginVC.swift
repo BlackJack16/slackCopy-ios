ab6488bd669461e4059c1d99c5400af6b3458b32
priyanto <16priyanto@gmail.com>
2018-05-27 12:57:01 +0700

    add close button func

---
+++ slackCopy-ios/slackCopy-ios/Controller/loginVC.swift
@@ -0,0 +1,22 @@
+//
+//  loginVC.swift
+//  slackCopy-ios
+//
+//  Created by Black Jack on 27/05/18.
+//  Copyright © 2018 priyanto. All rights reserved.
+//
+
+import UIKit
+
+class loginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func closeBtnPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
